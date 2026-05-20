3f7f114a39c42a4db6fe41311bb999c835242295
leduyphuongcm <leduyphuongsd001@gmail.com>
2026-05-20 21:43:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let total = 0;
 
+let history = [];
+
 function addMoney(){
 
 let money =
@@ -9,11 +11,59 @@ document.getElementById(
 ).value
 );
 
+if(!money){
+
+alert(
+"Nhập số tiền"
+);
+
+return;
+
+}
+
 total += money;
 
 document.getElementById(
-"result"
+"incomeText"
 ).innerText =
 total + "đ";
+
+history.push(
+"+ " +
+money +
+"đ"
+);
+
+updateHistory();
+
+document.getElementById(
+"money"
+).value = "";
+
+}
+
+function updateHistory(){
+
+let list =
+document.getElementById(
+"historyList"
+);
+
+list.innerHTML = "";
+
+history
+.slice()
+.reverse()
+.forEach(function(item){
+
+let li =
+document.createElement("li");
+
+li.innerText =
+item;
+
+list.appendChild(li);
+
+});
 
 }
